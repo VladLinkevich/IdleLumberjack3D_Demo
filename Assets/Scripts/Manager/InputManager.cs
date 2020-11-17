2555a76c408482b131772a98c14222fb6aa4aa34
VladLinkevich <linkevichvv@mail.ru>
2020-11-17 15:11:10 +0300

    added player movement and animation

---
+++ Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private Joystick joystick = null;
+
+    private void Update()
+    {
+        JoystickInput();
+        KeyboardInput();
+    }
+
+    private void JoystickInput()
+    {
+        Vector3 movement = Vector3.zero;
+
+        movement.x = joystick.Horizontal;
+        movement.z = joystick.Vertical;
+
+        if (movement.x != 0 || movement.y != 0)
+        {
+            Messenger<Vector3>.Broadcast(GameEvent.MOVE, movement);
+        }
+    }
+
+    private void KeyboardInput()
+    {
+        Vector3 movement = Vector3.zero;
+
+        float horInput = Input.GetAxis("Horizontal");
+        float verInput = Input.GetAxis("Vertical");
+
+        if (horInput != 0 || verInput != 0)
+        {
+            movement.x = horInput;
+            movement.z = verInput;
+            movement.Normalize();
+            Messenger<Vector3>.Broadcast(GameEvent.MOVE, movement);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 6f;
+
+    private CharacterController _charController;
+    private Animator _animator;
+
+    #region MonoBehaviour Callbacks
+
+    private void Awake()
+    {
+        Messenger<Vector3>.AddListener(GameEvent.MOVE, Move);
+    }
+
+    private void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _animator.SetFloat("Speed", Mathf.Abs(_charController.velocity.z + _charController.velocity.x));
+        _charController.Move(Vector3.zero);
+    }
+
+    #endregion
+   
+    private void Move(Vector3 mov)
+    {
+        _charController.Move(mov * moveSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(0, Mathf.Atan2(mov.x, mov.z) * Mathf.Rad2Deg, 0);
+    }
+
+}
